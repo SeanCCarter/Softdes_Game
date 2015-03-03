14f3fed5259bf14ee1315f76869d8abf5b3ae37f
SeanCCarter <seancarter26@gmail.com>
2015-03-03 14:34:46 -0500

    I think I finished the world module. Testing needed.

--- World_Module.py
+++ World_Module.py
@@ -16,13 +16,36 @@ class World(object):
 
 	def get_displayed_world(self, player_position, xsize, ysize):
 		self.load_world(player_position)
-		center_chunk = player_position[0]
-		
+		return [[self.get_square(player_position, dx, dy) for dx in xrange(-xsize/2, xsize/2+1)] for dy in xrange(-ysize/2, ysize/2+1)]]
+
+	def get_square(self, player_position, dx, dy):
+		'''Returns the block x distance and y distance from the player's square'''
+		block_chunk = player_position[0] #the block where the chunk should be located
+		x = player_position[1] #x location where the block should be located
+		y = player_position[2] #y location where the block should be located
+
+		#Updates the block location based on dx and dy
+		if x + dx < 0:
+			block_chunk[0] -= 1
+			x = self.chunk_width + dx + x 
+		elif x + dx > (self.chunk_width-1):
+			block_chunk[0] += 1
+			x = (dx + x)%self.chunk_width
+		if y + dy < 0:
+			block_chunk[1] -= 1
+			y = self.chunk_height + dy + y 
+		elif y + dy > (self.chunk_height-1):
+			block_chunk[1] += 1
+			y = (dy + y)%self.chunk_height
+
+		return self.loaded_world[block_chunk].get_block(x,y)
 
 	def load_world(self, player_position):
+		''' Checks the player position to see if the player is in the central chunk. If not, saves 
+			the chunks which are out of range, and loads new ones.'''
 		#Get the coordinates of the chunk the player is in
-		centerx = player_position[0]
-		centery = player_position[1]
+		centerx = player_position[0][0]
+		centery = player_position[0][1]
 
 		#Player has moved up, out of current center chunk
 		if (centerx, centery + 1) not in self.loaded_world:
@@ -109,7 +132,7 @@ class Chunk(object):
 			self.chunk = [row[:y_size+1] for row in chunk[:x_size+1]]
 			self.save_data()
 		else:
-			filename = "("+str(self.location[0])+str(self.location[1])+").txt"
+			filename = "./chunks/("+str(self.location[0])+str(self.location[1])+").txt"
 			data_file = open(filename, "r")
 			self.chunk = load(data_file)
 			data_file.close()
@@ -121,9 +144,12 @@ class Chunk(object):
 		dump(self.chunk)
 		data_file.close()
 
+	def get_block(x,y):
+		return self.chunk[x][y]
+
 
 	#Not reallly functions for the class. Not really sure where to put it yet.
-	def check_surroundings(x_coord, y_coord, value):
+	def check_surroundings(self, x_coord, y_coord, value):
 		"""
 		If the variable world has already been defined, it checks all x and y coords within one square (aka, checks the 8 surrounding squares) for a given value. If that value is present in 1 or more squares, returns True; else, False.
 		"""
@@ -136,7 +162,7 @@ class Chunk(object):
 					pass
 		return False
 
-	def make_blank_world(x_size, y_size):
+	def make_blank_world(self, x_size, y_size):
 		"""
 		Creates an x-by-y list of lists of zeroes.
 		"""
