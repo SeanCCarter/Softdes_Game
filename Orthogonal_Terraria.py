1eab04c26546cd274c371a01823afc59ce6f3ac6
SeanCCarter <user.email>
2015-03-12 00:12:01 -0400

    Removed unneccessary comments, added helpful ones

--- Orthogonal_Terraria.py
+++ Orthogonal_Terraria.py
@@ -67,32 +67,6 @@ class Game_Instance(object):
 		worldfile.close()
 		pygame.quit()
 
-# def main():
-# 	pygame.init()
-# 	SCREEN = pygame.display.set_mode((64*19,64*15))
-# 	font = pygame.font.SysFont("Arial",30)
-
-# 	world = World(chunk_width = 10, chunk_height = 10)
-# 	player1 = Player((0,0), 0, 0)
-# 	player1_controller = Arrow_Controller(player1, world)
-
-# 	while not player1_controller.exit_flag:
-# 		player1_controller.process_events()
-# 		# for event in pygame.event.get():
-# 		# 	if event.type == pygame.KEYDOWN:
-# 		# 		player1_controller.process_events()
-# 		label = font.render(str(player1),1,(0,0,0))
-# 		SCREEN.fill((255,255,255))
-
-# 		visible_world = world.get_displayed_world(player1.position, [-9,9], [-7,7])
-# 		for i in range(len(visible_world)):
-# 			for j in range(len(visible_world[0])):
-# 				SCREEN.blit(visible_world[i][j], (j*64, i*64))
-# 		SCREEN.blit(player1.current_avatar, (9*64, 7*64))
-# 		SCREEN.blit(label, (300,300))
-# 		pygame.display.flip()
-# 	pygame.quit()
-# 	# sys.exit()
 
 def main():
 	Game = Game_Instance(loadworld = True)
